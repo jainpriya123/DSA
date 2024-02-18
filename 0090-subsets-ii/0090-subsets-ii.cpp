c277e217f3edcd9405d3243a5b0f3da57f5c92a5
Priya Jain <65457280+jainpriya123@users.noreply.github.com>
2024-02-18 15:21:39 +0530

    Time: 5 ms (35.64%), Space: 8.7 MB (58.94%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;       //to store all susbets
+        vector<int> curr;         //to store current individual subset (that we will build)
+        sort(nums.begin(),nums.end());      //sort the array so that duplicates are adjacent 
+        helper(nums,ans,curr,0);       //we start from index 0
+        return ans;
+        
+    }
+    
+    void helper(vector<int>& nums, vector<vector<int>>& ans, vector<int>& curr, int idx){
+        ans.push_back(curr);       //we include current susbet into final ans
+        for(int i=idx;i<nums.size();i++){     //check for all possibilites
+            if(i>idx &&  nums[i]==nums[i-1]) continue;      //if duplicate then we continue
+            curr.push_back(nums[i]);     //we include nums[i] in current subset
+            helper(nums,ans,curr,i+1); 
+            curr.pop_back();         //to get subset without nums[i]
+        }
+    }  
+//     void solve(vector<int>& a, int n, set<vector<int>>&ans,vector<int>temp)
+//     {
+//         if(n==0){
+//             sort(temp.begin(),temp.end());
+//             ans.insert(temp);
+//             return;
+//         }
+//         solve(a,n-1,ans,temp);
+//         temp.push_back(a[n-1]);
+//         solve(a,n-1,ans,temp);
+//     }
+    
+//     vector<vector<int>> subsetsWithDup(vector<int>& a) {
+//         vector<vector<int>>ans;
+//         set<vector<int>>s;
+//         vector<int>temp;
+//         solve(a,a.size(),s,temp);
+//         for(auto x:s){
+//             ans.push_back(x);
+//         }
+//         return ans;
+//     }
+};
