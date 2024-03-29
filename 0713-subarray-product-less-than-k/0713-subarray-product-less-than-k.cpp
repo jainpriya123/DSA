c9d84ec590b3783ec938910ede99024f23c14ccf
Priya Jain <65457280+jainpriya123@users.noreply.github.com>
2024-03-29 12:13:39 +0530

    Time: 54 ms (69.68%), Space: 63.6 MB (56.79%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
-    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-        if (k<=1) return 0; // edge case
-        unsigned long long prod=1;
-        int n=nums.size(), l=0, cnt=0;
-        for(int r=0; r<n; r++){
-            prod*=nums[r];
-            while (prod >= k)
-                prod/=nums[l++];
-            cnt+=(r-l+1);//# of valid subarrays for [l, r] by varying l
+    int numSubarrayProductLessThanK(vector<int>& arr, int k) {
+        if(k==0) return 0;
+        
+        int i=0, prod=1;
+        int ans=0;
+        int n=arr.size();
+        for(int j=0;j<n;j++){
+            prod= prod*arr[j];
+            
+            while(i<=j && prod>=k){
+                prod=prod/arr[i];
+                i++;
+            }
+            ans+= j-i+1; 
         }
-        return cnt;
+        
+        return ans;
     }
 };
