191a0cccec264395ab80f567957d7d3802e9fa59
Priya Jain <65457280+jainpriya123@users.noreply.github.com>
2024-03-29 11:39:12 +0530

    Time: 355 ms (18.25%), Space: 148.6 MB (33.71%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    int maxSubarrayLength(vector<int>& nums, int k) {
+    int maxSubarrayLength(vector<int>& arr, int k) {
         int ans=0;
-        unordered_map<int,int>mp;
-        int n=size(nums);
-
-        for(int l=0,r=0;r<n;r++){
-            mp[nums[r]]++; //inserting value in window from right 
-        //if newly inserted element exceed frequencey poped it out from left   
-            if(mp[nums[r]]>k){
-                while(nums[l]!=nums[r]){
-                    mp[nums[l]]--;
-                    l++;
-                }
-                mp[nums[l]]--;
-                l++;
+        map<int,int>mp;
+        int n=arr.size();
+        int i=0,j=0;
+        for(j=0;j<n;j++){
+            mp[arr[j]]++;
+            while(i<=j && mp[arr[j]]>k){
+                mp[arr[i]]--;
+                i++;
             }
-            ans=max(ans,r-l+1);
+            
+            ans= max(ans, j-i+1);
         }
+        
         return ans;
     }
 };
