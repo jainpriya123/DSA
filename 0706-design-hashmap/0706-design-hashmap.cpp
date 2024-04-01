6391c2c59566c4a2883803ed551f23dd2670162a
Priya Jain <65457280+jainpriya123@users.noreply.github.com>
2024-04-01 06:21:50 +0530

    Time: 261 ms (5.87%), Space: 307.8 MB (8.47%) - LeetHub

---
+++ 0706-design-hashmap/0706-design-hashmap.cpp
@@ -0,0 +1,24 @@
+class MyHashMap {
+public:
+    std::vector<int>mp;
+    MyHashMap() {
+        mp.clear();
+       for(int i=0;i<1000001;i++){
+           mp.push_back(-1);
+       }
+       
+    }
+    
+    void put(int key, int value) {
+        mp[key]=value;
+    }
+    
+    int get(int key) {
+        return mp[key];
+    }
+    
+    void remove(int key) {
+        mp[key]=-1;
+    }
+};
+
