e21b70cc0565eaea9905672de610dae5380eac3a
Priya Jain <65457280+jainpriya123@users.noreply.github.com>
2024-04-11 11:19:31 +0530

    Time: 15 ms (52.18%), Space: 14.9 MB (10.03%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -21,25 +21,5 @@ vector<int> twoSum(vector<int>& a, int t) {
         }
         return ans;
     }
-    // vector<int> twoSum(vector<int>& a, int t) {
-    //     map<int,vector<int>>mp;
-    //     vector<int> v;
-
-    //     int n=a.size();
-    //     for(int i=0;i<n;i++){
-    //         mp[a[i]].push_back(i);
-    //     }
-
-    //     for(int i=0;i<n;i++){
-    //         if(mp.find(t-a[i])!=mp.end()){
-    //             if(mp[t-a[i]][0]!=mp[a[i]][0]){
-    //                 v.push_back(mp[t-a[i]][0]);
-    //                 v.push_back(mp[a[i]][0]);
-    //                 return v;
-    //             }
-    //         }
-    //     }
-    //     return mp[t/2];
-
-    // }
+    
 };
