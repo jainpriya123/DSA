e43599400756d69d034c3e14067fe1989411f476
Priya Jain <65457280+jainpriya123@users.noreply.github.com>
2024-04-30 23:23:00 +0530

    Time: 996 ms (26.27%), Space: 11.6 MB (44.26%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,29 +1,48 @@
 class Solution {
 public:
+    int dx[4] = {-1,0,1,0};
+    int dy[4] = {0,1,0,-1};
     
-    bool solve(vector<vector<char>>& grid, vector<vector<int>> &vis, int i, int j, int ind, string word)   //vector<vector<int>> &vis ko '&' ke saath use karo..nhi to time limit exceed ka problem aayega..  
-    {
-         if(ind==word.size()) return true;
-        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size() || vis[i][j]==1 || word[ind]!=grid[i][j]) return false;
-       
+    bool isvalid(vector<vector<char>>& arr, string word, vector<vector<int>>&vis, int i, int j, int w){
+        int n=arr.size();
+        int m= arr[0].size();
         
-        vis[i][j]=1;
-        
-        if(solve(grid,vis,i-1,j,ind+1,word)||solve(grid,vis,i+1 ,j,ind+1,word) || solve(grid,vis,i,j-1,ind+1,word)|| solve(grid,vis,i,j+1,ind+1,word)) return true;
-    vis[i][j]=0;
+        if(i>=0 && j>=0 && i<n && j<m && vis[i][j]==0 && word[w]==arr[i][j]){
+            return true;
+        }
         return false;
     }
     
-    bool exist(vector<vector<char>>& grid, string word) {
-        int n=grid.size();
-        int m=grid[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
+    bool solve(vector<vector<char>>& arr, string word, vector<vector<int>>&vis, int i, int j, int w){
+        vis[i][j] = 1;
+        // cout<<"Word "<<word[w]<<"  ";
+        // cout<<arr[i][j]<<" ";
+        if(w==word.length()-1) 
+            return true;
+        
+        
+        for(int k=0;k<4;k++){
+            if(isvalid(arr, word, vis, i+dx[k], j+dy[k], w+1)){
+                // cout<<"w "<<w<<endl;
+                if(solve(arr, word, vis, i+dx[k], j+dy[k], w+1))
+                    return true;
+            }
+        }
+        vis[i][j] = 0;
+        return false;
+    }
+    
+    bool exist(vector<vector<char>>& arr, string word) {
+        int n=arr.size();
+        int m= arr[0].size();
+        
+        vector<vector<int>>vis(n, vector<int>(m,0));
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(vis[i][j]==0)
-                {
-                    if(solve(grid,vis,i,j,0,word)==true) return true;
+                if(vis[i][j]==0 && word[0]==arr[i][j]){
+                    if(solve(arr, word, vis, i, j, 0)) 
+                        return true;
                 }
             }
         }
