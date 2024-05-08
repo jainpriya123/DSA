591559a8761740c547fe6bb5eb5be02818917adf
Priya Jain <65457280+jainpriya123@users.noreply.github.com>
2024-05-08 23:03:34 +0530

    Time: 62 ms (22.15%), Space: 53.3 MB (23.36%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
-    int firstMissingPositive(vector<int>& a) {
-        int n= a.size();
-        int i=0;
-        while(i<n){
-            if(a[i]>0 && a[i]<n+1 && a[i]!=i+1 && a[i]!=a[a[i]-1]){
-                swap(a[i], a[a[i]-1]);
-            }
-            else{
+    int firstMissingPositive(vector<int>& arr) {
+        int n=arr.size();
+        
+        for(int i=0;i<n;){
+            if(arr[i]<=0 || arr[i]>n || arr[i]-1==i || arr[i]==arr[arr[i]-1]){
                 i++;
+                continue;
             }
+            
+            swap(arr[i], arr[arr[i]-1]);
         }
         
+        // for(auto x: arr){
+        //     cout<<x<<" ";
+        // }
+        
         for(int i=0;i<n;i++){
-            if(i+1!=a[i]) return i+1;
+            if(arr[i]!=i+1){
+                return i+1;
+            }
         }
         return n+1;
     }
