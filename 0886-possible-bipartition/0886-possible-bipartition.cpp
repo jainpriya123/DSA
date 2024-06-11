3efdd20b865413dec290077149d7656815cae776
Priya Jain <65457280+jainpriya123@users.noreply.github.com>
2024-06-11 08:44:19 +0530

    Time: 740 ms (5.01%), Space: 265.2 MB (5.08%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.cpp
+++ 0886-possible-bipartition/0886-possible-bipartition.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
     bool possibleBipartition(int n, vector<vector<int>>& arr) {
-        map<int, vector<int>>mp;
         
-        for(auto x: arr){
-            mp[x[0]].push_back(x[1]);
-            mp[x[1]].push_back(x[0]);
+        map<int, vector<int>>adj;
+        
+        for(int i=0;i<arr.size();i++){
+            adj[arr[i][0]].push_back(arr[i][1]);
+            // adj[arr[i][1]].push_back(arr[i][0]);
         }
         
-        vector<int>vis(n+1,-1);
         
-        for(int i=1;i<n;i++){
-            if(vis[i]==-1){
-                queue<pair<int,int>>q;
-                q.push({i, 0});
-                vis[i]=0;
-                while(q.size()){
-                    int node = q.front().first;
-                    int group= q.front().second;
-                    q.pop();
-                    
-                    for(auto it: mp[node]){
-                        if(vis[it]==-1){
-                            vis[it]=1-vis[node];
-                            q.push({it, 1-vis[node]});
-                        }
-                        else if(vis[it]==vis[node])
-                            return false;
-                    }
+        
+        for(int i=1;i<=n;i++){
+        
+            vector<int>vis(n+1, -1);
+        queue<pair<int,int>>q;
+        
+        q.push({i, 0});
+        vis[i]=-1;
+        
+        while(q.size()){
+            int node= q.front().first;
+            int group = q.front().second;
+            q.pop();
+            
+            for(auto it: adj[node]){
+                if(vis[it]==-1){
+                    vis[it]= 1-group;
+                    q.push({it, vis[it]});
+                }
+                else if(vis[it]==vis[node]){
+                    return false;
                 }
             }
+        }
+        
         }
         
         return true;
