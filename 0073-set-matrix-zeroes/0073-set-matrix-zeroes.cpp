811bf77dac3c47d5055a5b794c93aa4820858d87
Priya Jain <65457280+jainpriya123@users.noreply.github.com>
2025-01-03 14:18:50 +0530

    Time: 0 ms (100%), Space: 18.6 MB (33.37%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,36 +4,98 @@ public:
         int n=arr.size();
         int m=arr[0].size();
         
-        vector<int>row(n, -1);
-        vector<int>col(m,-1);
+        // vector<int>row(n, -1);
+        // vector<int>col(m,-1);
         
         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         if(arr[i][j]==0){
+        //             row[i]=0;
+        //             col[j]=0;
+        //         }
+        //     }
+        // }
+        
+        // for(int i=0;i<n;i++){
+        //     if(row[i]==0){
+        //         for(int j=0;j<m;j++){
+        //             arr[i][j]=0;
+        //         }
+        //     }
+        // }
+        
+        // for(int j=0;j<m;j++){
+        //     if(col[j]==0){
+        //         for(int i=0;i<n;i++){
+        //             arr[i][j]=0;
+        //         }
+        //     }
+        // }
+        
+        // return;
+        int row=-1;
+        int col=-1;
+        for(int j=1;j<m;j++){
+            if(arr[0][j]==0){
+                row=0;
+            }
+        }
+
+        for(int i=1;i<n;i++){
+            if(arr[i][0]==0){
+                col=0;
+            }
+        }
+
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
                 if(arr[i][j]==0){
-                    row[i]=0;
-                    col[j]=0;
+                    arr[i][0]=0;
+                    arr[0][j]=0;
                 }
             }
         }
-        
-        for(int i=0;i<n;i++){
-            if(row[i]==0){
-                for(int j=0;j<m;j++){
+
+        for(int j=1;j<m;j++){
+            if(arr[0][j]==0){
+                for(int i=1;i<n;i++){
                     arr[i][j]=0;
                 }
             }
         }
         
-        for(int j=0;j<m;j++){
-            if(col[j]==0){
-                for(int i=0;i<n;i++){
+        for(int i=1;i<n;i++){
+            if(arr[i][0]==0){
+                for(int j=1;j<m;j++){
                     arr[i][j]=0;
                 }
             }
         }
-        
+
+        if(arr[0][0]==0){
+            for(int i=0;i<n;i++){
+                arr[i][0]=0;
+            }
+
+            for(int j=0;j<m;j++){
+                arr[0][j]=0;
+            }
+        }
+
+        if(row==0){
+            for(int j=0;j<m;j++){
+                arr[0][j]=0;
+            }
+        }
+
+        if(col==0){
+            for(int i=0;i<n;i++){
+                arr[i][0]=0;
+            }
+
+        }
         return;
-        
     }
 };
